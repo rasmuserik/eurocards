3ec3a5efd2633fe33c6d52a1f39160609ee54af7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-06-19 23:18:59 +0200

    ...

--- process.js
+++ process.js
@@ -1,4 +1,5 @@
 var countries, obj, fs, lines, i, j;
+require('child_process').exec('mkdir out');
 fs = require('fs');
 lines = fs.readFileSync('data.txt', 'utf8').split('\n');
 countries = [];
@@ -17,6 +18,28 @@ for(i=0;lines[i];i+=8) {
     obj.fertilityRate = f(7);
     countries.push(obj);
 }
+
+countries.forEach(function(d) {
+    var Canvas = require('canvas')
+      , canvas = new Canvas(200,200)
+      , ctx = canvas.getContext('2d');
+
+    //var flag = new Canvas.Image;
+    //flag.src = fs.readFileSync('flags/' + d.id + '-lgflag.png');
+    //ctx.drawImage(flag, 0, 0, flag.width, flag.height);
+    
+    ctx.font = '30px Impact';
+    ctx.rotate(.1);
+    ctx.fillText("Awesome!", 50, 100);
+    var te = ctx.measureText('Awesome!');
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.beginPath();
+    ctx.lineTo(50, 102);
+    ctx.lineTo(50 + te.width, 102);
+    ctx.stroke();
+
+    fs.writeFile('out/'+d.id+'.png', canvas.toBuffer());
+});
 
 /* LAZY, just copied from StackOverflow */
 function toTitleCase(str) { return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}); }
