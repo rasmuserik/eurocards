f77129e6b004b34099c965bb1b8b30383bd3de8e
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-06-20 10:15:13 +0200

    ...

--- process.js
+++ process.js
@@ -19,7 +19,20 @@ for(i=0;lines[i];i+=8) {
     obj.fertilityRate = f(7);
     countries.push(obj);
 }
-countries = [countries[3]];
+//countries = [countries[3]];
+
+function num(n) {
+    return addCommas(n);
+}
+function darkColor() {
+    var colors = ['#900', '#090', '#009', '#055', '#505', '#550'];
+    return colors[Math.random() * colors.length | 0];
+}
+function lightColor() {
+    return "#fff";
+    var colors = ['#800', '#080', '#008', '#088', '#808', '#880'];
+    return colors[Math.random() * colors.length | 0];
+}
 
 countries.forEach(function(d) {
     console.log(d);
@@ -40,37 +53,91 @@ countries.forEach(function(d) {
     ctx.fillStyle = '#fff';
     ctx.fillRect(40,40,745,1045);
 
+    var top = 90;
+    var lpos = 125;
     var map = new Image;
     map.src = fs.readFileSync('maps/' + d.id + '_large_locator.png');
-    ctx.drawImage(map, 80, 180, 500, 400);
+    ctx.drawImage(map, lpos, top + 100, 500, 400);
     
     ctx.fillStyle = '#000';
-    ctx.fillRect(80,80,1000,100);
-    ctx.fillStyle = '#fff';
+    ctx.fillRect(90,top,1000,100);
+    ctx.fillStyle = lightColor();
     ctx.font = '60px Impact';
-    ctx.fillText(d.name, 100, 150);
+    ctx.fillText(d.name, lpos, top + 70);
 
     var flag = new Image;
     flag.src = fs.readFileSync('flags/' + d.id + '-lgflag.png');
-    ctx.drawImage(flag, 450, 160, 300, 300*flag.height/flag.width);
+    ctx.drawImage(flag, 455, top + 80, 280, 280*flag.height/flag.width);
 
-    var basepos = 645;
+    var basepos = top + 565;
     var step = 55;
     var pos = -1;
+    var rpos = 700;
     ctx.fillStyle = '#000';
     ctx.font = '40px Sans-Serif';
-    ctx.fillText('Population:', 80, basepos + step * ++pos);
-    ctx.fillText('Highest point:', 80, basepos + step * ++pos);
-    ctx.fillText('GDP per person:', 80, basepos + step * ++pos);
-    ctx.fillText('Life expectancy:', 80, basepos + step * ++pos);
-    ctx.fillText('Area per person:', 80, basepos + step * ++pos);
-    ctx.fillText('Young (<15yrs.):', 80, basepos + step * ++pos);
-    ctx.fillText('Children per woman:', 80, basepos + step * ++pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#800';
+    ctx.fillText('Population:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(num(d.population), rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#080';
+    ctx.fillText('Highest point:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(num(d.highestPoint) + 'm', rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#008';
+    ctx.fillText('GDP per person:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText('$' + num(d.gdp), rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#000';
+    ctx.fillText('Life expectancy:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(d.lifeExpectancy + '', rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#088';
+    ctx.fillText('Area per person:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(num(1000000 * d.area / d.population | 0) + 'm²', rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#808';
+    ctx.fillText('Young (< 15 years):', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(d.children + '%', rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
+    ctx.fillStyle = '#880';
+    ctx.fillText('Children per woman:', lpos, basepos + step * ++pos);
+    ctx.textAlign = 'right';
+    ctx.fillText(d.fertilityRate, rpos, basepos + step * pos);
+    ctx.textAlign = 'left';
+
 
     fs.writeFileSync('out/'+d.id+'.png', canvas.toBuffer());
 });
 
 /* LAZY, just copied from StackOverflow */
 function toTitleCase(str) { return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}); }
+/* LAZY, just copied from http://www.mredkj.com/javascript/numberFormat.html */
+function addCommas(nStr)
+{
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
 
 //console.log(countries);
